ff203066e0a9bfc5ef28956a16a3ebb45bc2f59d
CodeWithFranco <120446758+CodeWithFranco@users.noreply.github.com>
2023-12-01 06:30:40 -0800

    Random direction with different colored lines

--- main.py
+++ main.py
@@ -1,84 +1,24 @@
 from turtle import Turtle, Screen
+import random
 
 # import turtle as t
 ninja_turtle = Turtle()
 ninja_turtle.shape("turtle")
 
-"""
-0deg = west
-45deg = south east
-90deg = south
-135deg = southwest
-180deg = west
-225deg = northwest
-270deg =  north
-"""
+colors = ["royal blue", "forest green", "goldenrod", "pale green", "sandy brown"]
+direction = [0, 90, 180, 270]
+width = [5, 6, 7, 10]
+
+for i in range(500):
+    ninja_turtle.color(random.choice(colors))
+    ninja_turtle.forward(30)
+    ninja_turtle.setheading(random.choice(direction))
+    ninja_turtle.pensize(random.choice(width))
 
 
-def triangle():
-    ninja_turtle.color("blue")
-    for i in range(3):
-        ninja_turtle.forward(100)
-        ninja_turtle.right(120)
 
 
-def square():
-    ninja_turtle.color("gray")
-    for i in range(4):
-        ninja_turtle.forward(100)
-        ninja_turtle.right(90)
 
-
-def pentagon():
-    ninja_turtle.color("green")
-    for i in range(5):
-        ninja_turtle.forward(100)
-        ninja_turtle.right(360 / 5)
-
-
-def hexagon():
-    ninja_turtle.color("red")
-    for i in range(6):
-        ninja_turtle.forward(100)
-        ninja_turtle.right(360 / 6)
-
-
-def heptagon():
-    ninja_turtle.color("pink")
-    for i in range(7):
-        ninja_turtle.forward(100)
-        ninja_turtle.right(360 / 7)
-
-
-def octagon():
-    ninja_turtle.color("purple")
-    for i in range(8):
-        ninja_turtle.forward(100)
-        ninja_turtle.right(360 / 8)
-
-
-def nonagon():
-    ninja_turtle.color('maroon')
-    for i in range(9):
-        ninja_turtle.forward(100)
-        ninja_turtle.right(360 / 9)
-
-
-def decagon():
-    ninja_turtle.color("green")
-    for i in range(10):
-        ninja_turtle.forward(100)
-        ninja_turtle.right(360 / 10)
-
-
-triangle()
-square()
-pentagon()
-hexagon()
-heptagon()
-octagon()
-nonagon()
-decagon()
 
 screen = Screen()
 screen.exitonclick()
